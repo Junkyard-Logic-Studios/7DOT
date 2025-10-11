18dd443d0f3c8753e381b61751a25e14b92e2c6c
Martin Werner <m.werner.0@protonmail.com>
2025-10-11 12:42:27 +0200

    Add trial implementation for connecting a single gamepad

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <memory>
 #include <stdexcept>
-#include <SDL3/SDL.h>
-#include <SDL3/SDL_vulkan.h>
+#include "SDL3/SDL.h"
 
 
 class SDLException final : public std::runtime_error
@@ -18,18 +17,18 @@ class Game
 public:
 	Game()
 	{
-		if (not SDL_Init(SDL_INIT_VIDEO))
+		if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD))
 			throw SDLException("Failed to init SDL");
-		if (not SDL_Vulkan_LoadLibrary(nullptr))
-			throw SDLException("Failed to load Vulkan");
 
-		auto* window = SDL_CreateWindow("Game", 800, 600,
-			SDL_WINDOW_VULKAN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIDDEN);
-		if (not window)
-			throw SDLException("Failed to create window");
+		SDL_Window* window;
+		SDL_Renderer* renderer;
+		if (!SDL_CreateWindowAndRenderer("Game", 800, 600, SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIDDEN, &window, &renderer))
+			throw SDLException("Failed to create window and renderer");
 
 		_window.reset(window);
+		_renderer.reset(renderer);
 
+		SDL_ShowWindow(_window.get());
 	}
 
 	~Game()
@@ -37,23 +36,57 @@ public:
 		SDL_Quit();
 	}
 
-	void run()
+	bool update()
 	{
-		SDL_ShowWindow(_window.get());
-		_running = true;
-		while (_running)
+		// poll SDL events
+		for (SDL_Event event; SDL_PollEvent(&event);)
 		{
-			for (SDL_Event event; SDL_PollEvent(&event);)
+			if (event.type == SDL_EVENT_QUIT)
+				return false;
+
+			if (event.type == SDL_EVENT_GAMEPAD_ADDED)
 			{
-				if (event.type == SDL_EVENT_QUIT)
-					_running = false;
+				if (_gamepad) continue; 	// already assigned
+
+				auto* gamepad = SDL_OpenGamepad(event.gdevice.which);
+				if (!gamepad)
+					throw SDLException("Failed to open gamepad");
+
+				_gamepad.reset(gamepad);
+			}
+
+			if (event.type == SDL_EVENT_GAMEPAD_REMOVED)
+			{
+				if (_gamepad && (SDL_GetGamepadID(_gamepad.get()) == event.gdevice.which))
+					_gamepad.release();
 			}
 		}
+
+		// render
+		int winw = 640, winh = 480;
+		const char* text = "Plug in a joystick, please.";
+		float x, y;
+
+		if (_gamepad)
+			text = SDL_GetGamepadName(_gamepad.get());
+
+		SDL_SetRenderDrawColor(_renderer.get(), 0, 0, 0, 255);
+		SDL_RenderClear(_renderer.get());
+		SDL_GetWindowSize(_window.get(), &winw, &winh);
+
+		x = (((float) winw) - (SDL_strlen(text) * SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE)) / 2.0f;
+		y = (((float) winh) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE) / 2.0f;
+		SDL_SetRenderDrawColor(_renderer.get(), 255, 255, 255, 255);
+		SDL_RenderDebugText(_renderer.get(), x, y, text);
+		SDL_RenderPresent(_renderer.get());
+
+		return true;
 	}
 
 private:
 	std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)> _window {nullptr, SDL_DestroyWindow};
-	bool _running = false;
+	std::unique_ptr<SDL_Renderer, decltype(&SDL_DestroyRenderer)> _renderer {nullptr, SDL_DestroyRenderer};
+	std::unique_ptr<SDL_Gamepad, decltype(&SDL_CloseGamepad)> _gamepad {nullptr, SDL_CloseGamepad};
 };
 
 
@@ -61,8 +94,8 @@ int main()
 {
 	try
 	{
-		Game game {};
-		game.run();
+		Game game;
+		while (game.update());
 	}
 	catch (const std::exception& e)
 	{
